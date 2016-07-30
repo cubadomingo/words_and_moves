9895c6d3cf01581da36705a11fd07126f801c420
Devin Osorio <me@devinosor.io>
2016-07-29 22:09:59 -0400

    removes webpack file from .gitignore so travis can pass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,12 +3,6 @@
 // DO NOT REQUIRE jQuery or jQuery-ujs in this file!
 // DO NOT REQUIRE TREE!
 
-// CRITICAL that vendor-bundle must be BEFORE bootstrap-sprockets and turbolinks
-// since it is exposing jQuery and jQuery-ujs
-
-//= require vendor-bundle
-//= require app-bundle
-
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
