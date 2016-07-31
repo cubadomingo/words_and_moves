017b8ae861704d852c4d06d830b5b4ccac7a12e6
Thomas Baird <tbaird@mail.umw.edu>
2016-07-30 22:17:53 -0400

    Adding model specs for new methods

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-
+  let(:event) { FactoryGirl.create :event }
+  let(:user) { FactoryGirl.create :user }
+  describe "Methods" do
+    describe ".net_likes" do
+      it "returns 1" do
+        event.likes.create!(user_id: user.id)
+        expect(event.net_likes).to eq(1)
+      end
+      context "when there is 1 like and 1 dislike" do
+        it "returns 0" do
+          event.likes.create!(user_id: user.id)
+          event.dislikes.create!(user_id: user.id)
+          expect(event.net_likes).to eq(0)
+        end
+      end
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  
+  let(:post) { FactoryGirl.create :post }
+  let(:user) { FactoryGirl.create :user }
+  describe "Methods" do
+    describe ".net_likes" do
+      it "returns 1" do
+        post.likes.create!(user_id: user.id)
+        expect(post.net_likes).to eq(1)
+      end
+      context "when there is 1 like and 1 dislike" do
+        it "returns 0" do
+          post.likes.create!(user_id: user.id)
+          post.dislikes.create!(user_id: user.id)
+          expect(post.net_likes).to eq(0)
+        end
+      end
+    end
+  end
 end
