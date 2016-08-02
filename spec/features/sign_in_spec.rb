e771dac7ab91469144a19d5f6b24484cf883abee
Devin Osorio <me@devinosor.io>
2016-08-02 09:56:51 -0400

    fixes failing features specs

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,20 +3,24 @@ RSpec.feature "Sign in" do
   context "when using valid credentials" do
     it "signs in user successfully" do
       visit new_user_session_path
-      fill_in "user_email", with: user.email
-      fill_in "user_password", with: "testing"
-      click_button "Sign in"
-      # expect(page).to have_selector("p.notice", text: "Signed in successfully.")
-      expect(page).to have_current_path(root_path)
+      within ('.sign-in') do
+        fill_in "user_email", with: user.email
+        fill_in "user_password", with: "testing"
+        click_button "Sign in"
+      end
+      expect(page).to have_current_path(feed_path)
+      expect(page).to have_selector(".alert", text: "Signed in successfully.")
     end
   end
   context "when using invalid credentials" do
     it "does not sign in user" do
       visit new_user_session_path
-      fill_in "user_email", with: "bad_email@gmail.com"
-      fill_in "user_password", with: "badpassword"
-      click_button "Sign in"
-      expect(page).to have_selector(".alert", text: "Invalid Email or password.") 
+      within('.sign-in') do
+        fill_in "user_email", with: "bad_email@gmail.com"
+        fill_in "user_password", with: "badpassword"
+        click_button "Sign in"
+      end
+      expect(page).to have_selector(".alert", text: "Invalid Email or password.")
     end
   end
 end
