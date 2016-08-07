7b37d952d1990cab1ef741e4681079e18c09d913
Thomas Baird <tbaird@mail.umw.edu>
2016-08-07 15:59:32 -0400

    Editing post and event show pages. Adding jquery for comments. References #83

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
 //= require turbolinks
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require_tree .
