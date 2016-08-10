56b2c16ff9138ce9bfad729e1be442610e08f436
Devin Osorio <me@devinosor.io>
2016-08-10 00:01:47 -0500

    removes first and last name form User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810021306) do
+ActiveRecord::Schema.define(version: 20160810045805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -154,8 +154,6 @@ ActiveRecord::Schema.define(version: 20160810021306) do
     t.datetime "updated_at",                          null: false
     t.boolean  "admin"
     t.string   "username"
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "provider"
     t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
