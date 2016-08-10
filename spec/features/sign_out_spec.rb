fe7185713b5e58810968042c55e0687e5ea1e41a
Devin Osorio <me@devinosor.io>
2016-08-10 10:47:59 -0700

    more css changes

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -2,7 +2,6 @@ RSpec.feature "Sign Out" do
   context "when a user clicks sign out" do
     scenario "the user is signed out successfully" do
       user = FactoryGirl.create :user
-      # Required because signing out redirects to the first regions page
       region = FactoryGirl.create :region
       sign_in user
       visit root_path
