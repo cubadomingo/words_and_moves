89cdf566291982943bd21f9b4b74cbc64584d5bc
Devin Osorio <me@devinosor.io>
2016-11-05 06:59:09 -0700

    deletes hello-word example that installs when react-on-rails is generated

--- client/app/bundles/CommentBox/startup/clientRegistration.jsx
+++
@@ -1,5 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-import CommentBoxApp from './CommentBoxApp';
-
-// This is how react_on_rails can see the CommentBoxApp in the browser.
-ReactOnRails.register({ CommentBoxApp });
